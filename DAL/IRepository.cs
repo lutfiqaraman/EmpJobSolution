65a8754fc1be5a97615cde8cc3cb9ebbc83a9949
lutfiqaraman <lutfiqaraman@gmail.com>
2020-10-28 14:38:01 +0300

    Create a generic repository

---
+++ DAL/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public interface IRepository<Entity> where Entity: class
+    {
+        IEnumerable<Entity> GetAll();
+        Entity GetByID(object id);
+        void Insert(Entity entityToInsert);
+        void Update(Entity entityToUpdate);
+        void Delete(object id);
+        void Save();
+    }
+}
